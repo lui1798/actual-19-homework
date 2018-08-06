4665d1ed6973ed2ff8aa085f072ae0f3a7eaf860
wangyouwei2016 <13709821410@139.com>
2018-08-06 19:25:24 +0800

    Default Changelist

--- lesson01/wangyouwei/chengfa.py
+++ lesson01/wangyouwei/chengfa.py
@@ -4,7 +4,7 @@
 # @Author  : Uwei
 # @File    : chengfa.py
 
-#通过指定end参数的值，可以取消在末尾输出回车符，实现不换行111222AA33
+#通过指定end参数的值，可以取消在末尾输出回车符，实现不换行
 for i in range(1, 10):
     for j in range(1, i + 1):
         print('{}x{}={}'.format(i, j, i * j), end=' ')
