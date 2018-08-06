ce619b046c8ef7ab189f0853b341e32c848ed356
wangyouwei2016 <wangyouwei2001@163.com>
2018-08-06 18:11:18 +0800

    加注释1

--- lesson01/wangyouwei/chengfa.py
+++ lesson01/wangyouwei/chengfa.py
@@ -3,7 +3,7 @@
 # @Time    : 18-8-6 下午5:09
 # @Author  : Uwei
 # @File    : chengfa.py
-#通过指定end参数的值，可以取消在末尾输出回车符，实现不换行
+#通过指定end参数的值，可以取消在末尾输出回车符，实现不换行1
 for i in range(1, 10):
     for j in range(1, i + 1):
         print('{}x{}={}'.format(i, j, i * j), end=' ')
