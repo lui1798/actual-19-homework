7386a5e691361ebabf3bd78dcf4412a777581ae2
wangyouwei2016 <wangyouwei2001@163.com>
2018-08-06 17:14:39 +0800

    乘法

--- lesson01/wangyouwei/chengfa.py
+++ lesson01/wangyouwei/chengfa.py
@@ -3,7 +3,7 @@
 # @Time    : 18-8-6 下午5:09
 # @Author  : Uwei
 # @File    : chengfa.py
-for i in range(1,10):
-    for j in range(1,i+1):
-        print('{}x{}={}'.format(i,j,i*j),end=' ')
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print('{}x{}={}'.format(i, j, i * j), end=' ')
     print()
