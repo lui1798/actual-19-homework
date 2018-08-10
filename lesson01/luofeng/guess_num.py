caac9d1f84d1c56a7a4fdfc81fc219461456724d
ourshappiness <ourshappiness@users.noreply.github.com>
2018-08-10 14:05:20 +0800

    Update guess_num.py

--- lesson01/luofeng/guess_num.py
+++ lesson01/luofeng/guess_num.py
@@ -9,11 +9,11 @@
 import random
 
 # define variables
-count = 6
-random_num = 0
+count = 6 // 循环计数
+random_num = 0 // 随机数
 
 while count > 0:
-    count-=1
+    count-=1 // 循环计数递减，直到0不大于0后，break退出
     random_num = random.randint(1,101)
     guess_num = int(input("Please enter a number within 100：").strip())
     if guess_num == random_num:
