722963a676376bba1693ece8df1a70230773af11
wangyouwei2016 <wangyouwei2001@163.com>
2018-08-14 17:49:55 +0800

    加注释，未改update,bug

--- lesson02/wangyouwei/LoginAndInfo.py
+++ lesson02/wangyouwei/LoginAndInfo.py
@@ -3,23 +3,22 @@
 # @Time    : 18-8-14 上午9:33
 # @Author  : Uwei
 # @File    : LoginAndInfo.py
-password_list = {'admin': '1', 'wywmir': '1'}
-keyword_list = ['list', 'add', 'delete', 'update', 'find', 'exit']
+password_list = {'admin': '1', 'wywmir': '1'}   #定义一个用户名密码字典
+keyword_list = ['list', 'add', 'delete', 'update', 'find', 'exit']  #定义关键字的列表
 user_info = [  # list
     [1, 'wyw1', '132xxx', 'beijing'],
     [2, 'wyw2', '132xxx', 'beijing'],
-    [3, 'wyw3', '132xxx', 'shanghai'],
+    [5, 'wyw3', '132xxx', 'shanghai'],
 ]
 Tries = 3
 while Tries > 0:
     loginname = input('请输入登录名:')
     password = input('请输入密码:')
-    if loginname in password_list.keys() and password_list[loginname] == password:
+    if loginname in password_list.keys() and password_list[loginname] == password:  #判断登录名在字典中，并且输入的登录名对应的密码和输入的密码匹配
         break
-
-    elif Tries - 1 == 0:
+    elif Tries - 1 == 0:    #最后一次打印锁定
         print('用户被锁定')
-        pass
+        pass    #做点什么
         break
     else:
         print('用户名或密码错误，还有{}次机会'.format(Tries - 1))
@@ -29,7 +28,7 @@ while True:
     op = input('请输入关键字(list,add,delete,update,find,exit): ')
     if op in keyword_list:
         if op == 'list':
-            print(u'---------------------用户信息-------------------------\nid       name        tel             address ')
+            print(u'---------------------用户信息-------------------------\nid       name        tel             address ') #\n是打印回车
             for i in user_info:
                 for x in i:
                     print(x, '\t', '\t', end=' ')
@@ -46,7 +45,7 @@ while True:
                 user_list.insert(0, new_id)
                 user_info.append(user_list)
         elif op == 'delete':
-            uid = input('输入要删除的行id')
+            uid = input('输入要删除的行id:')
             for x in user_info:
                 if x[0] == int(uid):
                     user_info.remove(x)
@@ -58,7 +57,7 @@ while True:
                     user_listnew = newbody.split(' ')
                     user_listnew.insert(0, selectuid)
                     user_info[int(selectuid) - 1] = user_listnew
-                    print(user_info)
+#                    print(user_info)
         elif op == 'find':
             selectkey = input(u'请输入： 查找关键字 :')
             for i in user_info:  #解开下层列表
