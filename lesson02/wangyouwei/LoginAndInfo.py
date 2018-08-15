abb327c748a2e940c889562cbe9ac074b0c74209
wangyouwei2016 <wangyouwei2001@163.com>
2018-08-15 15:17:37 +0800

    修完bug，增加1个更新的方法

--- lesson02/wangyouwei/LoginAndInfo.py
+++ lesson02/wangyouwei/LoginAndInfo.py
@@ -3,22 +3,23 @@
 # @Time    : 18-8-14 上午9:33
 # @Author  : Uwei
 # @File    : LoginAndInfo.py
-password_list = {'admin': '1', 'wywmir': '1'}   #定义一个用户名密码字典
-keyword_list = ['list', 'add', 'delete', 'update', 'find', 'exit']  #定义关键字的列表
+password_list = {'admin': '1', 'wywmir': '1'}  # 定义一个用户名密码字典
+keyword_list = ['list', 'add', 'delete', 'update', 'find', 'exit']  # 定义关键字的列表
 user_info = [  # list
     [1, 'wyw1', '132xxx', 'beijing'],
-    [2, 'wyw2', '132xxx', 'beijing'],
+    [2, 'wyw2', '132xxx', 'sy'],
     [5, 'wyw3', '132xxx', 'shanghai'],
+    [6, 'wyw5', '132xxx', 'shanghai'],
 ]
 Tries = 3
 while Tries > 0:
     loginname = input('请输入登录名:')
     password = input('请输入密码:')
-    if loginname in password_list.keys() and password_list[loginname] == password:  #判断登录名在字典中，并且输入的登录名对应的密码和输入的密码匹配
+    if loginname in password_list.keys() and password_list[loginname] == password:  # 判断登录名在字典中，并且输入的登录名对应的密码和输入的密码匹配
         break
-    elif Tries - 1 == 0:    #最后一次打印锁定
+    elif Tries - 1 == 0:  # 最后一次打印锁定
         print('用户被锁定')
-        pass    #做点什么
+        pass  # 做点什么
         break
     else:
         print('用户名或密码错误，还有{}次机会'.format(Tries - 1))
@@ -28,7 +29,8 @@ while True:
     op = input('请输入关键字(list,add,delete,update,find,exit): ')
     if op in keyword_list:
         if op == 'list':
-            print(u'---------------------用户信息-------------------------\nid       name        tel             address ') #\n是打印回车
+            print(
+                u'---------------------用户信息-------------------------\nid       name        tel             address ')  # \n是打印回车
             for i in user_info:
                 for x in i:
                     print(x, '\t', '\t', end=' ')
@@ -41,26 +43,40 @@ while True:
                 user_info.append(user_list)  # 将输入的信息加到id 1 后面
             else:
                 uids = [x[0] for x in user_info]  # 列表推导式 第0个元素组成个新列表
-                new_id = max(uids) + 1
-                user_list.insert(0, new_id)
+                new_id = max(uids) + 1  #新的udi是最大的+1
+                user_list.insert(0, new_id) #在列表第0个违章插入新id
                 user_info.append(user_list)
         elif op == 'delete':
             uid = input('输入要删除的行id:')
             for x in user_info:
-                if x[0] == int(uid):
+                if x[0] == int(uid):    #判断输入的id等于第几行的第0个
                     user_info.remove(x)
         elif op == 'update':
-            selectuid = input(u'请输入： 行id :')
+            selectold = input(u'请输入关键字查找 :')
+            print(u'---------------------用户信息-------------------------\nid       name        tel             address ')  # \n是打印回车
+            for i in user_info:
+                if selectold in i:
+                    for x in i:
+                        print(x, '\t', '\t', end=' ')
+                        # print (user_info.index(i))
+                    print()
+            selectuid = input(u'请输入要修改的行id :')
+            newbody = input(u'请输入新信息： name tel address 用空格分隔 :')
+            newuser_list = selectuid.split(' ') + newbody.split(' ')    #输入的id转列表再合并
             for x in user_info:
                 if x[0] == int(selectuid):
-                    newbody = input(u'请输入新信息： name tel address 用空格分隔 :')
-                    user_listnew = newbody.split(' ')
-                    user_listnew.insert(0, selectuid)
-                    user_info[int(selectuid) - 1] = user_listnew
-#                    print(user_info)
+                    sid = user_info.index(x)    #找到输入id对应的列表索引
+                    user_info[sid] = newuser_list
+
+        #    方法2
+        #     id_list = [] 把id放到列表里
+        #     for i in user_info:
+        #         id_list.append(i[0])
+        #     select_index = id_list.index(int(selectuid)) #找到输入的id对应的列表索引
+        #     user_info[select_index] = newuser_list
         elif op == 'find':
             selectkey = input(u'请输入： 查找关键字 :')
-            for i in user_info:  #解开下层列表
+            for i in user_info:  # 解开下层列表
                 if selectkey in i:
                     print(i)
         elif op == 'exit':
