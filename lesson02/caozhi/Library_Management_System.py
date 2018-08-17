e3e37ca003f07af7288c89f777d0e5315115be95
caozhi <heyemmby@163.com>
2018-08-17 20:45:39 +0800

    caozhi update v1.3 homework

--- lesson02/caozhi/Library_Management_System.py
+++ lesson02/caozhi/Library_Management_System.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# author emmby,2018-08-15
-# 图书管理系统
+# author emmby,2018-08-15 version:1.3
+# 图书馆信息管理系统
 
 def login():
-    global user_name, password
+    global user_name, password, count
+    with open('/home/caozhi/file', 'r') as f:
+        count = int(f.read())
     if count == 0:
-        print('Please try tomorrow!')
+        print('Please contact super manager!')
         exit(1)
-    user_name = input('Please enter your user_name: ')
-    password = input('Please enter your password: ')
+    user_name = input('\033[33m Please enter your user_name: \033[0m')
+    password = input('\033[33m Please enter your password: \033[0m')
 print('=' * 80, end='')
 print('''
 \033[31mWelcome to come Liaoning Project Technology University library management system \033[0m
@@ -23,72 +25,76 @@ userinfo = [
     [5, 'xuwei', 50, '029', 'xian'],
     [8, 'pushu', 45, '010', 'beijing'],
 ]
-count = 3
 while 1:
     login()
     if user_name == usermessage[0] and password == usermessage[1]:
-        print('login success')
+        print('\033[32m login success \033[0m')
         print('=' * 80)
-        print('''
-Please enter an action
-    1、"insert": insert a record.
-    2、"select": select someone message.
-    3、"update": update someone message.
-    4、"delete": delete someone message.
-    5、"quit": quit this system.
-        ''')
-        print('=' * 80)
-        print()
-        action = input('Please enter your action: ')
-        if action == 'insert':
-            insert_id = userinfo[-1][0] + 1
-            insert_name = input('Please enter add name: ')
-            insert_age = int(input('Please enter add age: '))
-            insert_tel = input('Please enter add tel: ')
-            insert_add = input('Please enter add address: ')
-            userinfo.append([insert_id, insert_name, insert_age, insert_tel, insert_add])
-        elif action == 'select':
-            select_name = input('Please enter select name: ')
-            select_flag = 0
-            for i in userinfo:
-                if select_name == i[1]:
-                    print(i)
-                    select_flag = 1
-            if select_flag == 0:
-                print('Sorry, record empty')
-        elif action == 'update':
-            update_id = int(input('Please enter update student id: '))
-            update_flag = 0
-            j = 0
-            for m in userinfo:
-                if update_id == m[0]:
-                    update_name = input('Please enter update name: ')
-                    update_age = int(input('Please enter update age: '))
-                    update_tel = input('Please enter update tel: ')
-                    update_add = input('Please enter update address: ')
-                    update_flag = 0
-                    userinfo[j] = [update_id, update_name, update_age, update_tel, update_add]
-                    print(userinfo[j])
-                    update_flag = 1
-                j += 1
-            if update_flag == 0:
-                print('Sorry, record empty')
-        elif action == 'delete':
-            delete_id = int(input('Please enter delete student id: '))
-            n = 0
-            delete_flag = 0
-            for j in userinfo:
-                if delete_id == j[0]:
-                    userinfo.pop(n)
-                    delete_flag = 1
-                n += 1
-            if delete_flag == 0:
-                print('Sorry, record empty')
-        elif action == 'exit':
-            print('Exit success')
-            exit(0)
-        else:
-            print('Your action is illegal')
+        while 1:
+            print('''
+         Please enter an action
+         1、"insert": insert a record.
+         2、"select": select someone message.
+         3、"update": update someone message.
+         4、"delete": delete someone message.
+	 5、"show": show all message.
+         6、"quit": quit this system.
+                ''')
+            action = input('Please enter your action: ')
+            if action == 'insert':
+                insert_id = userinfo[-1][0] + 1
+                insert_name = input('Please enter add name: ')
+                insert_age = int(input('Please enter add age: '))
+                insert_tel = input('Please enter add tel: ')
+                insert_add = input('Please enter add address: ')
+                userinfo.append([insert_id, insert_name, insert_age, insert_tel, insert_add])
+                print(userinfo[-1])
+            elif action == 'select':
+                select_name = input('Please enter select name: ')
+                select_flag = 0
+                for i in userinfo:
+                    if select_name == i[1]:
+                        print(i)
+                        select_flag = 1
+                if select_flag == 0:
+                    print('Sorry, record empty')
+            elif action == 'update':
+                update_id = int(input('Please enter update student id: '))
+                update_flag = 0
+                j = 0
+                for m in userinfo:
+                    if update_id == m[0]:
+                        update_name = input('Please enter update name: ')
+                        update_age = int(input('Please enter update age: '))
+                        update_tel = input('Please enter update tel: ')
+                        update_add = input('Please enter update address: ')
+                        update_flag = 0
+                        userinfo[j] = [update_id, update_name, update_age, update_tel, update_add]
+                        print(userinfo[j])
+                        update_flag = 1
+                    j += 1
+                if update_flag == 0:
+                    print('Sorry, record empty')
+            elif action == 'delete':
+                delete_id = int(input('Please enter delete student id: '))
+                n = 0
+                delete_flag = 0
+                for j in userinfo:
+                    if delete_id == j[0]:
+                        userinfo.pop(n)
+                        delete_flag = 1
+                    n += 1
+                if delete_flag == 0:
+                    print('Sorry, record empty')
+            elif action == 'show':
+                print(userinfo)
+            elif action == 'quit':
+                print('Exit success')
+                exit(0)
+            else:
+                print('Your action is illegal')
     else:
         count -= 1
+        with open('/home/caozhi/file', 'w') as f:
+            f.write(str(count))
         print('login error, you have %d left choice' % count)
---
+++ lesson02/caozhi/README.md
@@ -0,0 +1,131 @@
+##############################   图书馆用户信息管理系统     ##############################
+##########################################################################################
+
+================================================================================
+Welcome to come Liaoning Project Technology University library management system
+================================================================================
+ Please enter your user_name: name
+ Please enter your password: passwd
+login error, you have 2 left choice
+ Please enter your user_name: who
+ Please enter your password: your
+login error, you have 1 left choice
+ Please enter your user_name: root
+ Please enter your password: 123456
+login error, you have 0 left choice
+Please contact super manager!
+[caozhi@vm-201808032303 caozhi]$ python Library_Management_System.py
+================================================================================
+Welcome to come Liaoning Project Technology University library management system
+================================================================================
+Please contact super manager!
+[caozhi@vm-201808032303 caozhi]$ echo 3 > /home/caozhi/file
+[caozhi@vm-201808032303 caozhi]$ python Library_Management_System.py
+================================================================================
+Welcome to come Liaoning Project Technology University library management system
+================================================================================
+ Please enter your user_name: admin
+ Please enter your password: playbook
+ login success
+================================================================================
+
+         Please enter an action
+         1、"insert": insert a record.
+         2、"select": select someone message.
+         3、"update": update someone message.
+         4、"delete": delete someone message.
+	 5、"show": show all message.
+         6、"quit": quit this system.
+
+Please enter your action: insert
+Please enter add name: xianwen
+Please enter add age: 26
+Please enter add tel: 13900888800
+Please enter add address: beijing
+[9, 'xianwen', 26, '13900888800', 'beijing']
+
+         Please enter an action
+         1、"insert": insert a record.
+         2、"select": select someone message.
+         3、"update": update someone message.
+         4、"delete": delete someone message.
+	 5、"show": show all message.
+         6、"quit": quit this system.
+
+Please enter your action: select
+Please enter select name: xianwen
+[9, 'xianwen', 26, '13900888800', 'beijing']
+
+         Please enter an action
+         1、"insert": insert a record.
+         2、"select": select someone message.
+         3、"update": update someone message.
+         4、"delete": delete someone message.
+	 5、"show": show all message.
+         6、"quit": quit this system.
+
+Please enter your action: show
+[[1, 'caozhi', 25, '0419', 'liaoyang'], [2, 'emmby', 25, '010', 'beijing'], [3, 'xiaozhi', 25, '0429', 'huludao'], [5, 'xuwei', 50, '029', 'xian'], [8, 'pushu', 45, '010', 'beijing'], [9, 'xianwen', 26, '13900888800', 'beijing']]
+
+         Please enter an action
+         1、"insert": insert a record.
+         2、"select": select someone message.
+         3、"update": update someone message.
+         4、"delete": delete someone message.
+	 5、"show": show all message.
+         6、"quit": quit this system.
+
+Please enter your action: update
+Please enter update student id: 9
+Please enter update name: dahuang
+Please enter update age: 28
+Please enter update tel: 132543654765
+Please enter update address: gansu
+[9, 'dahuang', 28, '132543654765', 'gansu']
+
+         Please enter an action
+         1、"insert": insert a record.
+         2、"select": select someone message.
+         3、"update": update someone message.
+         4、"delete": delete someone message.
+	 5、"show": show all message.
+         6、"quit": quit this system.
+
+Please enter your action: delete
+Please enter delete student id: 9
+
+         Please enter an action
+         1、"insert": insert a record.
+         2、"select": select someone message.
+         3、"update": update someone message.
+         4、"delete": delete someone message.
+	 5、"show": show all message.
+         6、"quit": quit this system.
+
+Please enter your action: show
+[[1, 'caozhi', 25, '0419', 'liaoyang'], [2, 'emmby', 25, '010', 'beijing'], [3, 'xiaozhi', 25, '0429', 'huludao'], [5, 'xuwei', 50, '029', 'xian'], [8, 'pushu', 45, '010', 'beijing']]
+
+         Please enter an action
+         1、"insert": insert a record.
+         2、"select": select someone message.
+         3、"update": update someone message.
+         4、"delete": delete someone message.
+	 5、"show": show all message.
+         6、"quit": quit this system.
+
+Please enter your action: quit
+Exit success
+
+#############################################################################################################
+#############################################################################################################
+=======================================     冒泡排序法     ==================================================
+
+input times: 5
+input a number: 8
+input a number: 88
+input a number: 999
+input a number: 6
+input a number: 4
+[4, 6, 8, 88, 999]
+
+############################################################################################################
