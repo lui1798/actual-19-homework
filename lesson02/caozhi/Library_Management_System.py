128be5cc9bff6aa1a0c37767fff2fb058b227641
caozhi <heyemmby@163.com>
2018-08-18 13:52:55 +0800

    xiaozhi update v2.2 homework

--- lesson02/caozhi/Library_Management_System.py
+++ lesson02/caozhi/Library_Management_System.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# author emmby,2018-08-15 version:1.3
+# author emmby,2018-08-15 version:2.1
 # 图书馆信息管理系统
 
 # 登陆函数，并判断是否失败超过3次，写入到文件
@@ -14,11 +14,6 @@ def login():
     user_name = input('\033[33m Please enter your user_name: \033[0m')
     password = input('\033[33m Please enter your password: \033[0m')
 
-print('=' * 80, end='')
-print('''
-\033[31mWelcome to come Liaoning Project Technology University library management system \033[0m
-''', end='')
-print('=' * 80)
 
 # 管理员用户和密码
 usermessage = ('admin', 'playbook')
@@ -33,22 +28,27 @@ userinfo = [
 
 while 1:
     login()
-
     if user_name == usermessage[0] and password == usermessage[1]:
         print('\033[32m login success \033[0m')
+
+        # 登陆欢迎信息
+        print('=' * 80)
+        print('''
+\033[31mWelcome to come Liaoning Project Technology University library management system \033[0m
+''')
         print('=' * 80)
         while 1:
             print('''
          Please enter an action:
          1、"insert": insert a record.
          2、"select": select someone message.
-         3、"update": update someone message.
-         4、"delete": delete someone message.
-         5、"show": show all message.
+         3、"show": show all message.
+         4、"update": update someone message.
+         5、"delete": delete someone message.
          6、"quit": quit this system.
                 ''')
             # 输入对用户信息的操作 按数据库逻辑实现,id 为主键
-            action = input('Please enter your action: ')
+            action = input('\033[34mPlease enter your action: \033[0m')
 
             if action == 'insert':
                 insert_id = userinfo[-1][0] + 1
@@ -57,6 +57,7 @@ while 1:
                 insert_tel = input('Please enter add tel: ')
                 insert_add = input('Please enter add address: ')
                 userinfo.append([insert_id, insert_name, insert_age, insert_tel, insert_add])
+                print()
                 print(userinfo[-1])
 
             elif action == 'select':
@@ -91,23 +92,42 @@ while 1:
                 delete_id = int(input('Please enter delete student id: '))
                 n = 0
                 delete_flag = 0
-                for j in userinfo:
-                    if delete_id == j[0]:
+                for k in userinfo:
+                    if delete_id == k[0]:
                         userinfo.pop(n)
                         delete_flag = 1
                     n += 1
                 if delete_flag == 0:
                     print('Sorry, record empty')
 
+            # 显示用户信息分页展示，默认每页显示3条
             elif action == 'show':
-                for x in userinfo:
-                    print(x)
+                if len(userinfo) % 3 == 0:
+                    max_page = (len(userinfo) // 3)
+                else:
+                    max_page = (len(userinfo) // 3 + 1)
+                while 1:
+                    page = int(input('which page (0 for all): '))
+                    if 0 < page <= max_page:
+                        for m in userinfo[3 * (page - 1):3 * page]:
+                            print(m)
+                    elif page == 0:
+                        for a in userinfo:
+                            print(a)
+                    else:
+                        print('out of data,please enter correct page. eg:1 -- %d ' % max_page)
+                    print()
+                    show_quit = input('Weather continue show (\'N\' is quit): ')
+                    if show_quit == 'N':
+                        break
 
             elif action == 'quit':
+                print()
                 print('Exit success')
                 exit(0)
 
             else:
+                print()
                 print('Your action is illegal')
 
     else:
