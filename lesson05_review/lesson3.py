5f691770142db3382d435cad0fb8fe4b61bb5148
zhengyansheng <zhengyansheng@vipkid.com.cn>
2018-09-08 18:29:53 +0800

    add lesson05_review

---
+++ lesson05_review/lesson3.py
@@ -0,0 +1,38 @@
+
+
+
+# def f1():
+#     int('abc')
+#     for x in range(10):
+#         print(x)
+#
+#
+#
+# while 1:
+#     msg = input("please input: ")
+#     if msg == '1':
+#         print("hello world")
+#     else:
+#         f1()
+#         print("over")
+
+#
+# try:
+#     try:
+#         int('abc')
+#     except:
+#         print("int error")
+#     else:
+#         print("not except")
+#     finally:
+#         int('nihao')
+#         print("end")
+# except:
+#     print("Out except")
+#
+
+
+with open('README.md111', 'w') as fd:
+    fd.write('123')
+    fd.close()
+
