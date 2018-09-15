09243577f37a774c67e3643964e3e4e9ba172636
zhengyscn <zhengyscn@gmail.com>
2018-09-15 09:14:08 +0800

    Update func

--- lesson06/zhengyscn/func/main.py
+++ lesson06/zhengyscn/func/main.py
@@ -3,20 +3,16 @@ import sys
 import logging
 
 from utils import db
-from utils.fmt import Println
-from utils.file import ReadConfigFile
-
-
-
-
+from utils.fmt import Println, PrintTable
+from utils.file import ReadConfigFile, WriteExcel
 
 
 def main():
 	logging.basicConfig(level=logging.DEBUG,
 	                    format='[%(asctime)s] - [%(threadName)5s] - [%(filename)s-line:%(lineno)d] [%(levelname)s] %(message)s',
 	                    filename='/var/log/agent.log',
-						filemode = 'a'
-						)
+	                    filemode='a'
+	                    )
 	'''
 	# Insert
 	insert_sql = "INSERT INTO user(username, age, tel, address) VALUES('monkey1', 18, '132xxx', 'beijing')"
@@ -46,10 +42,17 @@ def main():
 	print(msg)
 	'''
 	
+	# data = [
+	# 	{'id': 1, 'name': 'monkey1', 'age': 18, 'tel': '132xxx', 'address': 'beijing'},
+	# 	{'id': 2, 'name': 'monkey2', 'age': 18, 'tel': '132xxx', 'address': 'beijing'},
+	# 	{'id': 3, 'name': 'monkey3', 'age': 18, 'tel': '132xxx', 'address': 'beijing'},
+	# ]
 	
+	# data1 = {'id': 1, 'name': 'monkey1', 'age': 18, 'tel': '132xxx', 'address': 'beijing'}
+	# table_msg, ok = PrintTable(data1)
+	# print(table_msg)
 	
-	
-
+	# WriteExcel('python19.xls', data)
 
 
 if __name__ == '__main__':
