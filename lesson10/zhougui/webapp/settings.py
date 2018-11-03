0d32477a6cf0c9edf6598bed2574fd1aaa63b45e
root@51stack <root@51stack.com>
2018-11-03 10:02:27 +0800

    第十节课作业

--- lesson10/zhougui/webapp/settings.py
+++ lesson10/zhougui/webapp/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'work',
         'USER': 'root',
-        'PASSWORD': 'zgblogstack',
+        'PASSWORD': '*',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS':{
